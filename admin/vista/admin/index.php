469fc650f0db30595f02f483acc3cd64959a5399
David Morales <damoralesr97@outlook.com>
2019-05-17 16:01:05 -0500

    Admin, paginas y funcionalidades listas

--- admin/vista/admin/index.php
+++ admin/vista/admin/index.php
@@ -6,55 +6,81 @@
 ?>
 <!DOCTYPE html>
 <html>
+    <?php
+        $codigo_admin = $_GET['codigo_admin'];
+    ?>
     <head>
         <meta charset="UTF-8">
-        <title>Gestion de usuarios</title>
+        <title>Sistema de Gestion de Mensajes Electronicos</title>
     </head>
     <body>
-        <table border="1px">
-            <tr>
-                <th>Cedula</th>
-                <th>Nombres</th>
-                <th>Apellidos</th>
-                <th>Direccion</th>
-                <th>Telefono</th>
-                <th>Correo</th>
-                <th>Fecha Nacimiento</th>
-                <th>Eliminar</th>
-                <th>Modificar</th>
-                <th>Cambiar contrasena</th>
-            </tr>
+        <nav>
+            <ul>
+                <li><a href="index.php?codigo_admin=<?php echo $codigo_admin ?>">Inicio</a></li>
+                <li><a href="usuarios.php?codigo_admin=<?php echo $codigo_admin ?>">Usuarios</a></li>
+            </ul>
+        </nav>
+        <aside>
+            <p>Aqui va la foto</p>
+            <p>Aqui va el nombre del usuario</p>
+        </aside>
+        <section>
+            <h3>Mensajes Electronicos</h3>
+            <form>
+                <table border="1px">
+                    <tr>
+                        <th>Fecha</th>
+                        <th>Remite</th>
+                        <th>Destinatario</th>
+                        <th>Asunto</th>
+                        <th>Eliminar</th>
+                    </tr>
+                    <?php
+                        include '../../../config/conexionBD.php';
 
-            <?php
-                include '../../../config/conexionBD.php';
 
-                $sql = "SELECT * FROM usuario";
-                $result = $conn->query($sql);
+                        $sql = "SELECT * FROM correo";
+                        $result = $conn->query($sql);
+                        
 
-                if ($result->num_rows > 0){
-                    while($row = $result->fetch_assoc()){
-                        if($row["usu_eliminado"]!='S'){
-                            echo "<tr>";
-                            echo "<td>" .$row["usu_cedula"]."</td>";
-                            echo "<td>" .$row["usu_nombres"]."</td>";
-                            echo "<td>" .$row["usu_apellidos"]."</td>";
-                            echo "<td>" .$row["usu_direccion"]."</td>";
-                            echo "<td>" .$row["usu_telefono"]."</td>";
-                            echo "<td>" .$row["usu_correo"]."</td>";
-                            echo "<td>" .$row["usu_fecha_nacimiento"]."</td>";
-                            echo "<td><a href='eliminar.php?codigo=".$row['usu_codigo']."'>Eliminar</a></td>";
-                            echo "<td><a href='modificar.php?codigo=".$row['usu_codigo']."'>Modificar</a></td>";
-                            echo "<td><a href='cambiar_contrasena.php?codigo=".$row['usu_codigo']."'>Cambiar contrasena</a></td>";
+                        if ($result->num_rows > 0){
+                            while($row = $result->fetch_assoc()){
+                                if($row["cor_eliminado"]!='S'){
+                                    echo "<tr>";
+                                    echo "<td>" .$row["cor_fecha_envio"]."</td>";
+                                    echo "<td>".buscarCorreo($row["cor_usu_remite"])."</td>";
+                                    echo "<td>".buscarCorreo($row["cor_usu_destino"])."</td>";
+                                    echo "<td>" .$row["cor_asunto"]."</td>";
+                                    echo "<td><a href='eliminar_mensaje.php?codigo_mensaje=".$row['cor_codigo']."&codigo_admin=".$codigo_admin."'>Eliminar</a></td>";
+                                }
+                            }
+                        }else{
+                            echo "<td colspan=4>No hay mensajes electronicos</td>";
                         }
-                    }
-                }else{
-                    echo "<tr>";
-                    echo "<td colspan='7'>No existen usuarios registrados en el sistema</td>";
-                    echo "</tr>";
-                }
-                $conn->close();
-            ?>
-        </table>
-        <a href="../../../config/cerrar_sesion.php">Cerrar Sesion</a>
+
+                        function buscarCorreo($codigoCorreo){
+                            include '../../../config/conexionBD.php';
+                            $sql1 = "SELECT usu_correo FROM usuario WHERE usu_codigo='$codigoCorreo'";
+                            $result = $conn->query($sql1);
+                            if($result->num_rows > 0){
+                                while($row = $result->fetch_assoc()){
+                                    $direccionCorreo=$row["usu_correo"];
+                                }
+                            }
+                            return $direccionCorreo;
+                        }
+
+                        
+
+                        $conn->close();
+                    ?>
+                </table>
+            </form>
+        </section>
+        <footer>
+            <p>Copyright</p>
+            <p>David Andres Morales Rivera</p>
+            <p>2019</p>
+        </footer>
     </body>
 </html>
--- public/controladores/login.php
+++ public/controladores/login.php
@@ -15,7 +15,7 @@
         while($row = $result->fetch_assoc()){
             $codigo = $row["usu_codigo"];
             if ($row["rol_usu_codigo"]==1){
-                header("Location: ../../admin/vista/admin/index.php");
+                header("Location: ../../admin/vista/admin/index.php?codigo_admin=".$row['usu_codigo']);
             }else{
                 header("Location: ../../admin/vista/user/index.php?codigo=".$row['usu_codigo']);
             }
